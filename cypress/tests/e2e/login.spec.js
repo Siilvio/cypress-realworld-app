23bbb1f983f87e1e9d45ea638efbdb5d21a6bbc5
Silvio Barbosa <siilvio.barbosa@gmail.com>
2025-09-11 05:18:07 -0400

    atividade

--- cypress/tests/e2e/login.spec.js
+++ cypress/tests/e2e/login.spec.js
@@ -1,8 +1,31 @@
-describe('Login com sucesso', () => {
-  it('Deve fazer login com um usuário válido', () => {
+describe('Login', () => {
 
-        // cy.get("[id='username']").type('teste')
-        cy.get("[name='username']").type('teste')
+  it('Deve fazer login com um usuário válido', () => {
+    cy.visit('http://localhost:3000/signin')
+
+    cy.get('[name="username"]').type('teste')
+    cy.get('[id="password"]').type('asdasds')
+    cy.contains('button', 'Sign In').click()
+  });
+
+
+  it('Deve exibir uma mensagem de erro ao fazer login com credenciais inválidas', () => {
+    cy.visit('http://localhost:3000/signin')
+    cy.get('[name="username"]').type('teste')
+    cy.get('[id="password"]').type('123456')
+    cy.contains('button', 'Sign In').click()
+    cy.get('[data-test="signin-error"]').contains('Username or password is invalid')
+  });
+
+
+  it.only('Deve registrar um novo usuário com informações válidas', () => {
+    cy.visit('http://localhost:3000/signin')
+    cy.get('[data-test="signup"]').click()
+    
+    
     
   });
+
+
+
 });
